40fcff47f106dafaf0dcde59219f7da3751873e2
Jon Sharman <jon@freshworks.io>
2018-11-19 19:32:50 -0800

    Half way through 107 Time: 11:10

--- server/server.js
+++ server/server.js
@@ -1,20 +1,24 @@
+const http = require('http');
+const path = require('path');
 const express = require('express');
-
+const socketIO = require('socket.io');
 
 
 var app = express();
 const port = process.env.PORT || 3004;
-const path = require('path');
+
 const publicPath = path.join(__dirname, '/../public');
 
-app.use(express.static(publicPath));
 
+app.use(express.static(publicPath));
+var server = http.createServer(app);
+var io = socketIO(server);
 // app.get('/', (req,res) => {
 //   res.sendFile(publicPath);
   
 //   // res.send(req.user);
 // });
 
-app.listen(port, () =>{
+server.listen(port, () =>{
   console.log(`Server is up on port ${port}`);
 });
