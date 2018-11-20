2976a2135ff2feedfd97706d3c3d5b9fdde6155b
Jon Sharman <jon@freshworks.io>
2018-11-19 18:46:27 -0800

    A basic web page for node chat app

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+
+
+var app = express();
+const port = 3004;
+const path = require('path');
+const publicPath = path.join(__dirname, '/../public');
+
+app.use(express.static(publicPath));
+
+// app.get('/', (req,res) => {
+//   res.sendFile(publicPath);
+  
+//   // res.send(req.user);
+// });
+
+app.listen(port, () =>{
+  console.log(`Server is up on port ${port}`);
+});
