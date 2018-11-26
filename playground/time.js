6ad796297174037753bb321ee405feaffd202835
Jon Sharman <jon@freshworks.io>
2018-11-25 16:36:13 -0800

    Lecture 118: Format time stamps using moment

--- playground/time.js
+++ playground/time.js
@@ -1,5 +1,11 @@
 const moment = require('moment');
 
-let date = moment();
+
+var someTimestamp = moment().valueOf();
+console.log(someTimestamp);
+
+
+let createdAt = 1234;
+let date = moment(createdAt);
 date.add(1, 'year').subtract(9,'months');
 console.log(date.format('h:mm a'));
