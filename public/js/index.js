f0015377b65b6dc7e9ac70ed3350ec138f977142
Jon Sharman <jon@freshworks.io>
2018-11-25 20:29:47 -0800

    Lecture 119: Add mustache js for templates

--- public/js/index.js
+++ public/js/index.js
@@ -32,19 +32,28 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function(message) {
   let formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = jQuery('<li> </li>');
-  li.text(`${message.from} ${formattedTime}: ${message.text}`);
-  jQuery('#messages').append(li);
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template,{
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
+  jQuery('#messages').append(html);
+ 
+  // var li = jQuery('<li> </li>');
+  // li.text(`${message.from} ${formattedTime}: ${message.text}`);
+  // jQuery('#messages').append(li);
 })
 
 socket.on('newLocationMessage', function (message) {
   let formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = jQuery('<li></li>');
-  var a = jQuery('<a target ="_blank"> My current location </a>')
-  li.text(`${message.from} ${formattedTime}: `);
-  a.attr('href', message.url);
-  li.append(a);
-  jQuery('#messages').append(li);
+  let template = jQuery('#location-message-template').html();
+  let html = Mustache.render(template, {
+    url: message.url,
+    from: message.from,
+    createdAt: formattedTime
+  })
+  jQuery('#messages').append(html);
 });
 
 jQuery('#message-form').on('submit', function(e){
